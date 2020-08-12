edf6c8c260469fdcbfa0ad94b3f7ff053097ee46
oscargallon <oscar.gallon@free-now.com>
2020-08-12 16:36:33 +0200

    gettings covers in one request

--- core/src/main/java/com/oscarg798/amiibowiki/core/usecases/FillGameResultCoverUseCase.kt
+++ core/src/main/java/com/oscarg798/amiibowiki/core/usecases/FillGameResultCoverUseCase.kt
@@ -15,30 +15,40 @@ package com.oscarg798.amiibowiki.core.usecases
 import com.oscarg798.amiibowiki.core.models.GameSearchResult
 import com.oscarg798.amiibowiki.core.repositories.GameRepository
 import javax.inject.Inject
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
 
 class FillGameResultCoverUseCase @Inject constructor(private val gameRepository: GameRepository) {
 
-    suspend fun execute(gameSearchResults: Collection<GameSearchResult>): Collection<GameSearchResult> =
-        coroutineScope {
-            gameSearchResults.map { gameSearchResult ->
-                async(start = CoroutineStart.LAZY) {
-                    val cover = gameRepository.getGameCover(gameSearchResult.gameId)
-
-                    if (cover != null) {
-                        gameSearchResult.copy(coverUrl = transformImageUrl(cover))
-                    } else {
-                        gameSearchResult
-                    }
-                }
-            }.awaitAll()
+    suspend fun execute(gameSearchResults: Collection<GameSearchResult>): Collection<GameSearchResult> {
+        val gameIds = gameSearchResults.map { gameSearchResult ->
+            gameSearchResult.gameId
         }
+
+        val gameCoversMap = HashMap<Int, String>()
+        gameRepository.getGameCover(gameIds).map {
+            Pair(it.gameId, it.coverUrl)
+        }.forEach {
+            gameCoversMap[it.gameId] = it.coverUrl
+        }
+
+        return gameSearchResults.map {
+            val cover = gameCoversMap.get(it.gameId)
+            it.copy(
+                coverUrl = if (cover == null) {
+                    cover
+                } else {
+                    transformImageUrl(cover)
+                }
+            )
+        }
+    }
 
     private fun transformImageUrl(url: String) =
         url.replace(ORIGINAL_SCHEMA, DESIRED_SCHEMA).replace(ORIGINAL_IMAGE_SIZE, COVER_SIZE)
+
+    private val Pair<Int, String>.gameId
+        get() = this.first
+    private val Pair<Int, String>.coverUrl
+        get() = this.second
 }
 
 private const val COVER_SIZE = "t_cover_small"
